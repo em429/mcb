d1605ef8753a495c59da386f2451a137904f7de2
hajzso <hajzso@protonmail.com>
2017-02-06 22:07:30 +0100

    Changes arguments

--- mcb.py
+++ mcb.py
@@ -2,24 +2,20 @@
 # mcb.py | saves clipboard text under keywords, or loads a saved clipboard
 # state into the current clipboard.
 
-# Usage:
-# mv mcb.py /usr/local/bin/mcb
-# mcb save <keyword> - Saves clipboard contents to keyword.
-# mcb <keyword> - Loads keyword to clipboard.
-# mcb list - Prints all saved keywords.
-
 import sys, shelve, pyperclip
 
 shelfFile = shelve.open('mcbData')
 
-if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
-    shelfFile[sys.argv[2]] = pyperclip.paste()
-elif len(sys.argv) == 2:
+print(len(sys.argv))
+
+if len(sys.argv) == 2:
+    # mcb list -- list keywords
     if sys.argv[1].lower() == 'list':
         print(str(list(shelfFile.keys())))
-    elif sys.argv[1] in shelfFile:
+    elif sys.argv[1] == '-l' and sys.argv[2] in shelfFile:
+        print(shelfFile[sys.argv[2]])
         pyperclip.copy(shelfFile[sys.argv[1]])
     else:
-        print('Can\'t find requested keyword..')
+        shelfFile[sys.argv[1]] = pyperclip.paste()
 
 shelfFile.close()
